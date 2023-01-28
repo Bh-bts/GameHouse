ece3d4cbc8a9c4e417af7e49f35715c703157776
bhavin thumar <thumarbhavin.786@gmail.com>
2023-01-28 17:51:50 +0530

    deleted parallel.xml file

--- src/main/java/com/Game/testcase/One_To_Fifty_Page.java
+++ src/main/java/com/Game/testcase/One_To_Fifty_Page.java
@@ -1,15 +1,20 @@
 package com.Game.testcase;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
 import com.Browsers.Util.AllBrowsers;
 
 public class One_To_Fifty_Page extends AllBrowsers {
 
 	public void One_To_Page_Method() throws InterruptedException {
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		
 		for (int i = 1; i <= 50; i++) {
 
 			String numbers_xpath = "//div[text()='" + i + "']";
-
+			//js.executeScript("arguments[0].scrollIntoView(true);", numbers_xpath);
 			driver.findElement(By.xpath(numbers_xpath)).click();
 
 		}
